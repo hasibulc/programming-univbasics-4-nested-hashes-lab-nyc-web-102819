9860a935ddfa75003b28d2fc3f62dd805000d4bf
Hasibul Chowdhury <hasibulc91@gmail.com>
2019-10-24 00:08:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -41,17 +41,9 @@ def adding_matz
 
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
         }
      }
 
@@ -76,6 +68,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+    
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
      
      
 end
