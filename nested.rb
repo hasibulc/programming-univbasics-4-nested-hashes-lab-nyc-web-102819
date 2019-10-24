eb8918fa624c7f4ee46444b453113035c0c46c64
Hasibul Chowdhury <hasibulc91@gmail.com>
2019-10-24 00:10:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,8 +69,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
     
-    programmer_hash[:alan_kay][:known_for] = alans_new_info
-     
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  
+  programmer_hash 
      
 end
 
@@ -91,6 +92,9 @@ def adding_to_dennis
         }
      }
      
-    programmer_hash[:dennis_ritchie][:languages] += [""]
+  programmer_hash[:dennis_ritchie][:languages] += ["Assembly"]
+  
+  programmer_hash
+  
 
 end
